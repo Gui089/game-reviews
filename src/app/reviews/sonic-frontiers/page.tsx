441fe8e3df41f7e756248e755d264c9cd9af3358
Gui089 <guilhermesilba811@gmail.com>
2024-04-24 20:16:51 -0300

    Add sonic-frontiers review page

---
+++ src/app/reviews/sonic-frontiers/page.tsx
@@ -0,0 +1,12 @@
+
+
+const SonicReviewPage = () => {
+    return (
+        <>
+            <h1>Sonic Frontiers - Analise</h1>
+            <p>Analise do Sonic Frontiers aqui</p>
+        </>
+    )
+}
+
+export default SonicReviewPage;
