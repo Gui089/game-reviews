2bc2948961a00882af1467a5dfe1e6379b2683f4
Gui089 <guilhermesilba811@gmail.com>
2024-04-24 20:13:51 -0300

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+
+
+const AboutPage = () => {
+    return (
+        <>
+            <h1>Sobre o analise de jogos</h1>
+            <p>Descricao do analise de jogos</p>
+        </>
+    )
+}
+
+export default AboutPage;
