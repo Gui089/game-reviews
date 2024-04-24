f266ec621cbacd93a2b6d076b5c26ea8b04fdd29
Gui089 <guilhermesilba811@gmail.com>
2024-04-24 20:10:51 -0300

    Add page review do super mario bross wonder

---
+++ src/app/reviews/super-mario-bross-wonder/page.tsx
@@ -0,0 +1,12 @@
+
+
+const MarioWonderReview = () => {
+    return (
+        <>
+            <h1>Super mario Bros - analise</h1>
+            <p>Review do super mario Wonder Review</p>
+        </>
+    )
+}
+
+export default MarioWonderReview;
