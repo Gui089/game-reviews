d78a02da439b83d8f0d82df2cf51a16b37a92df0
Gui089 <guilhermesilba811@gmail.com>
2024-06-18 11:34:30 -0300

    :white_check_mark: Add: images in game reviews

--- src/app/analises/sonic-frontiers/page.tsx
+++ src/app/analises/sonic-frontiers/page.tsx
@@ -1,10 +1,18 @@
 import Heading1 from "@/Components/Heading1";
+import Image from "next/image";
 
 
 const SonicReviewPage = () => {
     return (
         <>
             <Heading1>Sonic Frontiers - Analise</Heading1>
+            <Image 
+                width='640'
+                height='360'
+                className="rounded-lg mt-5 mb-5" 
+                src="/images/sonic-frontiers.jpg"
+                alt="Logo do jogo" 
+                priority/>
             <p>Analise do Sonic Frontiers aqui</p>
         </>
     )
--- src/app/analises/super-mario-bross-wonder/page.tsx
+++ src/app/analises/super-mario-bross-wonder/page.tsx
@@ -1,10 +1,18 @@
 import Heading1 from "@/Components/Heading1";
+import Image from "next/image";
 
 
 const MarioWonderReview = () => {
     return (
         <>
             <Heading1>Super mario Bros - analise</Heading1>
+            <Image 
+                width='640' 
+                height='360' 
+                className="rounded-lg mt-5 mb-5"
+                src="/images/super-mario-bros-wonder.jpg" 
+                alt="Logo do jogo Super Mario Bors Wonder" 
+                priority/>
             <p>Review do super mario Wonder Review</p>
         </>
     )
